449077b5fcab26fdfe8aed52965c589ef9d0e575
Fane <nitus15@yahoo.com>
2023-02-20 16:07:38 +0200

    Added image processing

--- ball_tracker.py
+++ ball_tracker.py
@@ -1,2 +1,44 @@
 import cv2 as cv
-import control_motor
+import numpy as np
+import time
+
+cap = cv.VideoCapture(0)
+lower_hsv = np.array([28,47,47])
+high_hsv = np.array([43,255,255])
+
+
+while True:
+    ret, image = cap.read()
+    time.sleep(1/30)
+    if image is not None:
+        image = cv.resize(image,(300,300))
+        image_blurred = cv.GaussianBlur(image,(17,17),0)
+
+        imageHSV = cv.cvtColor(image_blurred,cv.COLOR_BGR2HSV)
+
+
+        thresh = cv.inRange(imageHSV,lower_hsv,high_hsv)
+        thresh = cv.erode(thresh ,None , iterations=5)
+        thresh = cv.dilate(thresh ,None , iterations=5)
+
+
+        contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            (x, y), radius = cv.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            cv.circle(image, center, radius, (0, 255, 0), 2)
+            cv.circle(image,center,radius=2,color=(255,0,0),thickness=2)
+            break
+
+        #cv.imshow("kek", image)
+        cv.imshow("kek", thresh)
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        print("Image is none")
+        break
+
+
+image.release()
+cv.destroyAllWindows()
