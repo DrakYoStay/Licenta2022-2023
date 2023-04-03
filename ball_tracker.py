8165c626c9bd6fdaea881e0e25501e521ca19ac3
Fane <nitus15@yahoo.com>
2023-04-03 18:03:16 +0300

    Revert "Added new tests"
    This reverts commit 82acb6bfdbb2dbf861029a166ec84224e6a8b5e9.

--- ball_tracker.py
+++ ball_tracker.py
@@ -11,44 +11,43 @@ lower_hsv = np.array([28,47,47])
 high_hsv = np.array([43,255,255])
 camera_center = 150
 center = (0,0)
-time.sleep(2)
 
 while True:
     ret, image = cap.read()
-    time.sleep(1/60)
+    time.sleep(1/30)
     if image is not None:
         image = cv.resize(image,(300,300))
-        image_blurred = cv.GaussianBlur(image,(11,11),0)
+        image_blurred = cv.GaussianBlur(image,(17,17),0)
 
         imageHSV = cv.cvtColor(image_blurred,cv.COLOR_BGR2HSV)
 
 
         thresh = cv.inRange(imageHSV,lower_hsv,high_hsv)
-        thresh = cv.erode(thresh ,None , iterations=2)
-        thresh = cv.dilate(thresh ,None , iterations=2)
+        thresh = cv.erode(thresh ,None , iterations=5)
+        thresh = cv.dilate(thresh ,None , iterations=5)
         x_camera, _ = center
         contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for contour in contours:
             (x, y), radius = cv.minEnclosingCircle(contour)
             center = (int(x), int(y))
             radius = int(radius)
-            cv.circle(image_blurred, center, radius, (0, 255, 0), 2)
-            cv.circle(image_blurred,center,radius=2,color=(255,0,0),thickness=2)
-            if x_camera >= camera_center - 70 and x_camera <= camera_center + 70:
+            cv.circle(image, center, radius, (0, 255, 0), 2)
+            cv.circle(image,center,radius=2,color=(255,0,0),thickness=2)
+            if x_camera >= camera_center - 20 and x_camera <= camera_center + 20:
                 print("Mergi in fata")
                 CONTROLLER.inainte()
-            elif x_camera < camera_center - 70:
+            elif x_camera < camera_center - 20:
                 print("Fa stanga")
                 CONTROLLER.stanga()
-            elif x_camera > camera_center + 70:
+            elif x_camera > camera_center + 20:
                 print("Fa dreapta")
                 CONTROLLER.dreapta()
             break
 
 
         print(x_camera)
-        cv.imshow("kek", image)
-        #cv.imshow("kek", thresh)
+        # cv.imshow("kek", image)
+        cv.imshow("kek", thresh)
 
 
 
